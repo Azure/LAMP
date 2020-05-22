cd1831640a30a52b3575400e71b3617edb062520
Asif iTalent <60592188+asift91@users.noreply.github.com>
2020-05-22 16:58:21 +0530

    updated moodle script url
    Updated moodle script url

--- scripts/moodle_main.sh
+++ scripts/moodle_main.sh
@@ -7,7 +7,7 @@ echo "decoded text : ${decotext}" >> /home/"${1}"/log.txt
 
 wget_script(){
   cd /home/"${1}"/ 
-  wget https://raw.githubusercontent.com/ummadisudhakar/LAMP/MAT-32/scripts/moodle_script.sh
+  wget https://raw.githubusercontent.com/ummadisudhakar/LAMP/ansible_playbook_mat32/scripts/moodle_script.sh
   sudo chown -R "${1}":"${1}" /home/"${1}"/moodle_script.sh
 }
 
