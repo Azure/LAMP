e5fe99f5624bf3dba04be368a711eabb4c243dd9
Asif iTalent <60592188+asift91@users.noreply.github.com>
2020-05-26 13:45:31 +0530

    Updated variables

--- scripts/woocommerce.sh
+++ scripts/woocommerce.sh
@@ -2,18 +2,20 @@
 
 wooco_URL: https://downloads.wordpress.org/plugin/woocommerce.4.0.1.zip
 wooco_path=/home/${2}
+web_root=/var/www/html
+
 echo "domain_name ${1}" >>${wooco_path}/log.txt
 echo "user_name ${2}" >>${wooco_path}/log.txt
 
 downloadwoocommerce(){
-  wget -p ${wooco_URL} /home/${1}/
+  wget -p ${wooco_URL} ${wooco_URL}/
 }
 
 extractfile(){
   sudo apt install unzip
   sudo unzip ${wooco_path}/downloads.wordpress.org/plugin/woocommerce.4.0.1.zip
-  sudo cp -rf ${wooco_path}/woocommerce /var/www/html/wordpress/wp-content/plugins/
+  sudo cp -rf ${wooco_path}/woocommerce ${web_root}/wordpress/wp-content/plugins/
   sudo rm -rf ${wooco_path}/woocommerce
 }
-downloadwoocommerce ${2}
-extractfile ${1} ${2} 
+downloadwoocommerce
+extractfile 
