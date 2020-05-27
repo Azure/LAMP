48664ea5b0249ba9411ad8cd7fa435abab02c3ab
Asif iTalent <60592188+asift91@users.noreply.github.com>
2020-05-27 19:56:52 +0530

    Updated woocommerce version number
    added global variables
    passing woocommerce version number

--- scripts/woocommerce.sh
+++ scripts/woocommerce.sh
@@ -5,6 +5,9 @@
 wooco_URL=${1} 
 wooco_path=/home/${2}
 web_root=/var/www/html
+wooco_version=${3}
+wooco_direc_name=downloads.wordpress.org
+wooco_plugin_path=plugin/woocommerce
 
 downloadwoocommerce(){
   wget -p ${wooco_URL} ${wooco_path}/
@@ -12,9 +15,11 @@ downloadwoocommerce(){
 
 extractfile(){
   sudo apt install unzip
-  sudo unzip ${wooco_path}/downloads.wordpress.org/plugin/woocommerce.4.0.1.zip
+  sudo unzip ${wooco_path}/${wooco_direc_name}/${wooco_plugin_path}.${wooco_version}.zip
   sudo cp -rf ${wooco_path}/woocommerce ${web_root}/wordpress/wp-content/plugins/
   sudo rm -rf ${wooco_path}/woocommerce
+  sudo rm -rf {wooco_path}/${wooco_direc_name}/
 }
-downloadwoocommerce  >> ${wooco_path}/log.txt
-extractfile  >> ${wooco_path}/log.txt
+
+downloadwoocommerce 
+extractfile 
