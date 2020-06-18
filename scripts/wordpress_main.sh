264dfa75bbc7b3d1bf128eb1f67e6bf97f816664
asift91 <asift@italentdigital.com>
2020-06-18 17:21:33 +0530

    updated the comments

--- scripts/wordpress_main.sh
+++ scripts/wordpress_main.sh
@@ -3,7 +3,7 @@
 # The following script will run at the time of template deployment and user input will be encoded.
 # Script will download the wordpress_script.sh as a raw content from GitHub.
 # Encoded input will be decoded and appended to the wordpress_script.sh for execution.
-# The output of the script would be run.sh file at /home/azureadmin(username)/
+# The output of the script would be written to run.sh file at /home/azureadmin(username)/
 
 decotext=`echo ${2} | base64 --decode`
 
