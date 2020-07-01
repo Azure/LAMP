a3eefa72b2af5451b0132a3ccbee75563ba2c3cc
sayali0512 <63386141+sayali0512@users.noreply.github.com>
2020-07-01 12:12:27 +0530

    Updated the parameters

--- scripts/wordpress_script.sh
+++ scripts/wordpress_script.sh
@@ -4,9 +4,9 @@
 # It will update groups_var/all file in playbook with the user inputs dynamically
 # It will execute ansible playbook for installing WordPress in host VM (controller VM)
 
-log_path=/home/${3}/var.txt
-home_path=/home/${3}
-vars_path=/home/${3}/wordpress/group_vars/all
+log_path=/home/${1}/var.txt
+home_path=/home/${1}
+vars_path=/home/${1}/wordpress/group_vars/all
 # wp_admin_password is the password for wordpress site
 wp_admin_password=$(</dev/urandom tr -dc _A-Z-a-z-0-9 | head -c8)
 wp_db_user_pass=$(</dev/urandom tr -dc _A-Z-a-z-0-9 | head -c8)
@@ -17,7 +17,7 @@ install_ansible() {
   sudo apt-get install ansible -y
 }
 configure_ansible() {
-  sudo chown -R ${2}:${2} ${home_path}/.ansible/cp
+  sudo chown -R ${1}:${1} ${home_path}/.ansible/cp
   echo "Configure ansible Ip is : ${1}" >> ${log_path}
   sudo chmod 777 /etc/ansible/hosts
   sudo echo -e "[webservers]\n${1}" >>/etc/ansible/hosts
@@ -35,11 +35,7 @@ wordpress_install() {
   sudo sed -i "s~wp_db_server_name: wordpress~wp_db_server_name: ${2}~" ${vars_path} 
   sudo sed -i "s~wp_db_user: wordpress~wp_db_user: ${3}~" ${vars_path} 
   sudo sed -i "s~wp_db_password: password~wp_db_password: ${4}~" ${vars_path}
-  sudo sed -i "s~vm_password: password~vm_password: ${6}~" ${vars_path}
-  sudo sed -i "s~vm_ip: IP~vm_ip: ${7}~" ${vars_path}
-  sudo sed -i "s~wp_db_name: wordpress~wp_db_name: ${8}~" ${vars_path}
-  sudo sed -i "s~wp_admin_password: ~wp_admin_password: ${wp_admin_password}~" ${vars_path}
-  sudo sed -i "s~wp_db_user_pass: ~wp_db_user_pass: ${wp_db_user_pass}~" ${vars_path}
+  sudo sed -i "s~wp_db_name: wordpress~wp_db_name: ${6}~" ${vars_path}
   ansible-playbook /home/${1}/wordpress/playbook.yml -i /etc/ansible/hosts -u ${1}
 }
 
@@ -47,9 +43,9 @@ wordpress_install() {
 sudo sed -i "s~#   StrictHostKeyChecking ask~   StrictHostKeyChecking no~" /etc/ssh/ssh_config 
 sudo systemctl restart ssh
 install_ansible >> ${log_path}
-configure_ansible ${1} ${3} >> ${log_path}
+#configure_ansible ${1} ${3} >> ${log_path}
 install_svn
-wordpress_install ${3} ${4} ${5} ${6} ${7} ${2} ${1} ${8} >> ${log_path}
+wordpress_install ${1} ${2} ${3} ${4} ${5} ${6} >> ${log_path}
 # Enable strict host key checking
 sudo sed -i "s~   StrictHostKeyChecking no~#   StrictHostKeyChecking ask~" /etc/ssh/ssh_config 
 sudo systemctl restart ssh
