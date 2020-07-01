0372a379d8c861ff85931ac0c4d65aafc73a4eff
asift91 <asift@italentdigital.com>
2020-07-01 18:35:44 +0530

    added wordpress and woocommerce installation scripts

--- scripts/helper_functions.sh
+++ scripts/helper_functions.sh
@@ -47,8 +47,13 @@ function get_setup_params_from_configs_json
     export nfsHaLbIP=$(echo $json | jq -r .fileServerProfile.nfsHaLbIP)
     export nfsHaExportPath=$(echo $json | jq -r .fileServerProfile.nfsHaExportPath)
     export nfsByoIpExportPath=$(echo $json | jq -r .fileServerProfile.nfsByoIpExportPath)
-    # passing php versions $phpVersion from UI
     export phpVersion=$(echo $json | jq -r .phpProfile.phpVersion)
+    export applicationType=$(echo $json | jq -r .applicationProfile.applicationType)
+    export sshUsername=$(echo $json | jq -r .applicatioProfile.sshUsername)
+    export lbDns=$(echo $json | jq -r .applicationProfile.lbDns)
+    export applicationDbName=$(echo $json | jq -r .applicationProfile.applicationDbName)
+    export wpAdminPass=$(echo $json | jq -r .applicationProfile.wpAdminPass)
+    export wpDbUserPass=$(echo $json | jq -r .applicationProfile.wpDbUserPass)
 }
 
 function get_php_version {
@@ -81,6 +86,182 @@ function install_php_mssql_driver
     echo "extension=pdo_sqlsrv.so" >> /etc/php/$PHPVER/cli/php.ini
 }
 
+# function clonerepo
+# {
+#     cd /home/$sshUsername/ 
+#     wget https://raw.githubusercontent.com/Azure/LAMP/master/scripts/wordpress_script.sh
+#     sudo chown -R $sshUsername:$sshUsername /home/$sshUsername/wordpress_script.sh
+# }
+
+# function install_ansible {
+#   apt-add-repository ppa:ansible/ansible -y
+#   apt-get update
+#   apt-get install ansible -y
+# }
+
+# function install_svn {
+#   apt-get update -y
+#   apt-get install -y subversion
+# }
+
+function create_database
+{
+    local dbIP=$1
+    local dbadminloginazure=$2
+    local dbadminpass=$3
+    local applicationDbName=$4
+    local wp_db_user_id=$5
+    local wp_db_user_pass=$6
+
+    # create database for application
+    mysql -h $dbIP -u $dbadminloginazure -p$dbadminpass -e "CREATE DATABASE $applicationDbName CHARACTER SET utf8;"
+    # grant user permission for database
+    mysql -h $dbIP -u $dbadminloginazure -p$dbadminpass -e "GRANT ALL ON $applicationDbName.* TO $wp_db_user_id IDENTIFIED BY '$wp_db_user_pass';"
+
+}
+
+function download_wordpress
+{
+    local wordpress_path=/azlamp/html
+    local siteFQDN=$1
+    /usr/bin/curl -k --max-redirs 10 https://wordpress.org/latest.zip -L -o $wordpress_path/wordpress.zip
+    /usr/bin/unzip -q $wordpress_path/wordpress.zip
+    rm $wordpress_path/wordpress.zip
+    mv $wordpress_path/wordpress $wordpress_path/$siteFQDN
+}
+
+function create_wpconfig
+{
+    local dbIP=$1
+    local applicationDbName=$2
+    local dbadminloginazure=$3
+    local dbadminpass=$4
+    local siteFQDN=$5
+
+    cat <<EOF > /azlamp/html/$siteFQDN/wp-config.php
+  <?php
+  /**
+  * Following configration file will be updated in the wordpress folder in runtime 
+  *
+  * Following configurations: Azure Database for MySQL server settings, Table Prefix,
+  * Secret Keys, WordPress Language, and ABSPATH. 
+  * 
+  * wp-config.php  file is used during the installation.
+  * Copy the wp-config file to wordpress folder.
+  *
+  */
+
+  // ** Azure Database for MySQL server settings - You can get the following details from Azure Portal** //
+  /** Database name for WordPress */
+  define('DB_NAME', '$applicationDbName');
+
+  /** username for MySQL database */
+  define('DB_USER', '$dbadminloginazure');
+
+  /** password for MySQL database */
+  define('DB_PASSWORD', '$dbadminpass');
+
+  /** Azure Database for MySQL server hostname */
+  define('DB_HOST', '$dbIP');
+
+  /** Database Charset to use in creating database tables. */
+  define('DB_CHARSET', 'utf8');
+
+  /** The Database Collate type. Don't change this if in doubt. */
+  define('DB_COLLATE', '');
+
+  /**
+  * Authentication Unique Keys and Salts.
+  * You can generate unique keys and salts at https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service
+  * You can change these at any point in time to invalidate all existing cookies.
+  */
+
+  define('AUTH_KEY',         'h|Eu6ge.=Ej?fyV]/sHw:Ur~>(tkhZH(S^I[DHjE+OD}^MsG\`j0a/y8.n]@L8P{o');
+  define('SECURE_AUTH_KEY',  '\`D2d-b,i1YmFQqOy/^]#p_G^fSXWyPm]e:)}H~BVIG\`>vG\$AnnYqUj^#*pPB;*,j');
+  define('LOGGED_IN_KEY',    'Wqfh/&|XT| \$o0xeb+%Xf|_N;9Dpp19nzlB& b4w0I.D1;q<|-{4ajT\$JT(QF<@6');
+  define('NONCE_KEY',        '89mwVzZXf2-[qjN+k-]#lbg8+>gxO%Fso9;-ptiUwqFS_4x-u\$6I<d,~v=mF2__|');
+  define('AUTH_SALT',        't#ry@FfhD3,Y(lZf7+*V,&5rs(&\$xP,tz6[*<_&&CJW/]?2![NEQhsIi2vm-NYlZ');
+  define('SECURE_AUTH_SALT', '9zIjp#dKMLLi{&Ag[Ig0Q]oP[[jN qNz<_Z= Gx#Ig/mi>k-J(oE6Prr&L[zR5Vp');
+  define('LOGGED_IN_SALT',   '+(B*,@@5eH<?Mq7t-04>b>F%~C!6,+g?vf:w8N(Ne+nwA85N^U54#LHhssf1=>ap');
+  define('NONCE_SALT',       'drEw_Z[MD z7Jv,t;WuR8&Q #z? D0c8RR!v*~mkSW1-PlXa9Bl>5&b|=Xe{z9a^');
+
+
+  /**
+  * WordPress Database Table prefix.
+  *
+  * You can have multiple installations in one database if you give each a unique prefix.
+  * Only numbers, letters, and underscores are allowed.
+  */
+  $table_prefix  = 'wp_';
+
+  /**
+  * WordPress Localized Language, defaults language is English.
+  *
+  * A corresponding MO file for the chosen language must be installed to wp-content/languages. 
+  */
+  define('WPLANG', '');
+
+  /**
+  * For developers: Debugging mode for WordPress.
+  * Change WP_DEBUG to true to enable the display of notices during development.
+  * It is strongly recommended that plugin and theme developers use WP_DEBUG in their development environments.
+  */
+  define('WP_DEBUG', false);
+
+  /** Disable Automatic Updates Completely */
+  define( 'AUTOMATIC_UPDATER_DISABLED', True );
+
+  /** Define AUTOMATIC Updates for Components. */
+  define( 'WP_AUTO_UPDATE_CORE', True );
+
+  /** Absolute path to the WordPress directory. */
+  if ( !defined('ABSPATH') )
+    define('ABSPATH', dirname(__FILE__) . '/');
+
+  /** Sets up WordPress vars and included files. */
+  require_once(ABSPATH . 'wp-settings.php');
+
+  /** Avoid FTP credentails. */
+  define('FS_METHOD','direct');
+
+EOF
+}
+
+function install_wp_cli
+{
+    local sshUsername=$1
+    cd /home/$sshUsername
+    wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+    chmod +x /home/$sshUsername/wp-cli.phar
+    mv /home/$sshUsername/wp-cli.phar /usr/local/bin/wp
+}
+
+function install_wordpress
+{
+    local lbDns=$1
+    local wp_title=$2
+    local wp_admin_user=$3
+    local wp_admin_password=$4
+    local wp_admin_email=$5
+    local wp_path=$6
+
+    wp core install --url='https://$lbDns' --title='$wp_title' --admin_user='$wp_admin_user' --admin_password='$wp_admin_password' --admin_email='$wp_admin_email' --path='$wp_path'
+}
+
+function install_woocommerce
+{
+    local path=$1
+    wp plugin install woocommerce --path='$path'
+    wp plugin activate woocommerce --path='$path'
+}
+function install_sslcerts
+{
+    local path=/azlamp/certs/$1
+    mkdir $path
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $path/nginx.key -out $path/nginx.crt -subj "/C=US/ST=WA/L=Redmond/O=IT/CN=$1"
+    chown www-data:www-data $path/nginx.*
+    chmod 400 $path/nginx.*
+}
 function check_fileServerType_param
 {
     local fileServerType=$1
@@ -435,7 +616,14 @@ server {
         error_log /var/log/nginx/error.log;
 
         root ${htmlRootDir};
-        location ~* \.php$ {
+        #location ~* \.php$ {
+        #above line is replaced with below line according to moodle docs http://docs.moodle.org/38/en/Nginx
+        #location ~ ^(.+\.php)(.*)$ {  - This line also works fine
+        
+        location ~ [^/]\.php(/|$) {
+          fastcgi_split_path_info  ^(.+\.php)(.*)$;
+          fastcgi_param   PATH_INFO   \$fastcgi_path_info;
+
           include fastcgi_params;
           # Remove X-Powered-By, which is an information leak
           fastcgi_hide_header X-Powered-By;
@@ -466,7 +654,11 @@ server {
         error_log /var/log/nginx/error.log;
  
         root ${htmlRootDir};
-        location ~* \.php$ {
+        #location ~* \.php$ {
+        #above line is replaced with below line according to moodle docs http://docs.moodle.org/38/en/Nginx
+        #location ~ ^(.+\.php)(.*)$ {  - This line also works fine
+        
+        location ~ [^/]\.php(/|$) {
           include fastcgi_params;
           # Remove X-Powered-By, which is an information leak
           fastcgi_hide_header X-Powered-By;
@@ -593,7 +785,11 @@ server {
         error_log /var/log/nginx/error.log;
 
         root ${htmlRootDir};
-        location ~* \.php$ {
+        #location ~* \.php$ {
+        #above line is replaced with below line according to moodle docs http://docs.moodle.org/38/en/Nginx
+        #location ~ ^(.+\.php)(.*)$ {  - This line also works fine
+        
+        location ~ [^/]\.php(/|$) {
           include fastcgi_params;
           # Remove X-Powered-By, which is an information leak
           fastcgi_hide_header X-Powered-By;
@@ -624,7 +820,11 @@ server {
         error_log /var/log/nginx/error.log;
  
         root ${htmlRootDir};
-        location ~* \.php$ {
+        #location ~* \.php$ {
+        #above line is replaced with below line according to moodle docs http://docs.moodle.org/38/en/Nginx
+        #location ~ ^(.+\.php)(.*)$ {  - This line also works fine
+        
+        location ~ [^/]\.php(/|$) {
           include fastcgi_params;
           # Remove X-Powered-By, which is an information leak
           fastcgi_hide_header X-Powered-By;
@@ -709,7 +909,7 @@ function create_per_site_sql_db_from_controller
     local dbIP=${2}
     local dbadminloginazure=${3}
     local dbadminpass=${4}
-    local azlampdbname=${5}
+    local azlampdbname=${5} 
     local azlampdbuser=${6}
     local azlampdbpass=${7}
     local mssqlDbSize=${8}
