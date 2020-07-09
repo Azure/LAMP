4a9937615c23f8ab6941343aa3abbc6ead1d6eea
asift91 <asift@italentdigital.com>
2020-07-09 14:19:29 +0530

    updated the wordpress version

--- scripts/helper_functions.sh
+++ scripts/helper_functions.sh
@@ -56,6 +56,7 @@ function get_setup_params_from_configs_json() {
   export applicationDbName=$(echo $json | jq -r .applicationProfile.applicationDbName)
   export wpAdminPass=$(echo $json | jq -r .applicationProfile.wpAdminPass)
   export wpDbUserPass=$(echo $json | jq -r .applicationProfile.wpDbUserPass)
+  export wpVersion=$(echo $json | jq -r .applicationProfile.wpVersion)
 }
 
 function get_php_version() {
@@ -101,6 +102,19 @@ function create_database() {
   mysql -h $dbIP -u $dbadminloginazure -p$dbadminpass -e "GRANT ALL ON $applicationDbName.* TO $wpDbUserId IDENTIFIED BY '$wpDbUserPass';"
 }
 
+function download_wordpress_version() {
+  local wordpressPath=/azlamp/html
+  #local path=/var/lib/waagent/custom-script/download/0
+  local siteFQDN=$1
+  local version=$2
+
+  cd $wordpressPath
+  wget https://wordpress.org/wordpress-$version.tar.gz
+  tar -xvf $wordpressPath/wordpress-$version.tar.gz
+  rm $wordpressPath/wordpress-$version.tar.gz
+  mv $wordpressPath/wordpress $wordpressPath/$siteFQDN
+}
+
 function download_wordpress() {
   local wordpressPath=/azlamp/html
   #local path=/var/lib/waagent/custom-script/download/0
@@ -132,32 +146,24 @@ function create_wpconfig() {
   * Copy the wp-config file to wordpress folder.
   *
   */
-
   // ** Azure Database for MySQL server settings - You can get the following details from Azure Portal** //
   /** Database name for WordPress */
   define('DB_NAME', '$applicationDbName');
-
   /** username for MySQL database */
   define('DB_USER', '$dbadminloginazure');
-
   /** password for MySQL database */
   define('DB_PASSWORD', '$dbadminpass');
-
   /** Azure Database for MySQL server hostname */
   define('DB_HOST', '$dbIP');
-
   /** Database Charset to use in creating database tables. */
   define('DB_CHARSET', 'utf8');
-
   /** The Database Collate type. Don't change this if in doubt. */
   define('DB_COLLATE', '');
-
   /**
   * Authentication Unique Keys and Salts.
   * You can generate unique keys and salts at https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service
   * You can change these at any point in time to invalidate all existing cookies.
   */
-
   define('AUTH_KEY',         'h|Eu6ge.=Ej?fyV]/sHw:Ur~>(tkhZH(S^I[DHjE+OD}^MsG\`j0a/y8.n]@L8P{o');
   define('SECURE_AUTH_KEY',  '\`D2d-b,i1YmFQqOy/^]#p_G^fSXWyPm]e:)}H~BVIG\`>vG\$AnnYqUj^#*pPB;*,j');
   define('LOGGED_IN_KEY',    'Wqfh/&|XT| \$o0xeb+%Xf|_N;9Dpp19nzlB& b4w0I.D1;q<|-{4ajT\$JT(QF<@6');
@@ -166,8 +172,6 @@ function create_wpconfig() {
   define('SECURE_AUTH_SALT', '9zIjp#dKMLLi{&Ag[Ig0Q]oP[[jN qNz<_Z= Gx#Ig/mi>k-J(oE6Prr&L[zR5Vp');
   define('LOGGED_IN_SALT',   '+(B*,@@5eH<?Mq7t-04>b>F%~C!6,+g?vf:w8N(Ne+nwA85N^U54#LHhssf1=>ap');
   define('NONCE_SALT',       'drEw_Z[MD z7Jv,t;WuR8&Q #z? D0c8RR!v*~mkSW1-PlXa9Bl>5&b|=Xe{z9a^');
-
-
   /**
   * WordPress Database Table prefix.
   *
@@ -175,37 +179,29 @@ function create_wpconfig() {
   * Only numbers, letters, and underscores are allowed.
   */
   \$table_prefix  = 'wp_';
-
   /**
   * WordPress Localized Language, defaults language is English.
   *
   * A corresponding MO file for the chosen language must be installed to wp-content/languages. 
   */
   define('WPLANG', '');
-
   /**
   * For developers: Debugging mode for WordPress.
   * Change WP_DEBUG to true to enable the display of notices during development.
   * It is strongly recommended that plugin and theme developers use WP_DEBUG in their development environments.
   */
   define('WP_DEBUG', false);
-
   /** Disable Automatic Updates Completely */
   define( 'AUTOMATIC_UPDATER_DISABLED', True );
-
   /** Define AUTOMATIC Updates for Components. */
   define( 'WP_AUTO_UPDATE_CORE', True );
-
   /** Absolute path to the WordPress directory. */
   if ( !defined('ABSPATH') )
     define('ABSPATH', dirname(__FILE__) . '/');
-
   /** Sets up WordPress vars and included files. */
   require_once(ABSPATH . 'wp-settings.php');
-
   /** Avoid FTP credentails. */
   define('FS_METHOD','direct');
-
 EOF
 }
 
@@ -228,7 +224,7 @@ function install_wordpress() {
   wp core install --url=https://$lbDns --title=$wpTitle --admin_user=$wpAdminUser --admin_password=$wpAdminPassword --admin_email=$wpAdminEmail --path=$wpPath --allow-root
 }
 
-function install_plugins() {
+function install_plugin() {
   local path=$1
   wp plugin install woocommerce --path=$path --allow-root
   wp plugin activate woocommerce --path=$path --allow-root
